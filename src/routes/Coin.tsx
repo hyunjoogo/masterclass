a50ff0d2da996aae87e19c25fdb93bb08c04c63b
HyunJoo Kim <hyunjoogo@gmail.com>
2021-11-13 04:11:20 +0900

    4.4강

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,13 +1,47 @@
-import {useParams} from "react-router-dom";
+import {useLocation, useParams} from "react-router-dom";
+import styled from "styled-components";
+import {useState} from "react";
 
 interface RouterParams {
   coinId : string;
 }
 
+interface QueryParams {
+  name : string;
+}
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${props => props.theme.accentColor}
+`;
+
 export default () => {
+  const [loading, setLoading] = useState(true);
   const {coinId} = useParams<RouterParams>();
+  const {state} = useLocation<QueryParams>();
 
-  console.log(coinId)
-
-  return <h1>Coin</h1>
+  return <Container>
+    <Header>
+      <Title>{state?.name || "Loading..."}</Title>
+    </Header>
+    {loading ? <Loader>Loading...</Loader> : null}
+  </Container>
 }
