973f3319ec96b46028d3b50dda86e38990fbf216
HyunJoo Kim <hyunjoogo@gmail.com>
2022-01-11 14:12:05 +0900

    #6.13 final styles

--- src/Trello/components/Board.tsx
+++ src/Trello/components/Board.tsx
@@ -3,14 +3,38 @@ import DraggableCard from "./DraggableCard";
 import styled from "styled-components";
 
 const Wrapper = styled.ul`
-  padding: 20px 10px;
-  padding-top: 30px;
+  paddin-top: 10px;
   background-color: #3F8CF2;
   border-radius: 5px;
+  min-height: 300px;
+  display: flex;
+  flex-direction: column;
 `;
 const Title = styled.h1`
-  margin-bottom: 10px;
+  margin-top: 10px;
+  font-size: 18px;
+  text-align: center;
+  font-weight: 600;
+  color: white;
 `;
+
+interface IAreaProps {
+  isDraggingOver: boolean,
+  isDraggingFromThis: boolean
+}
+
+const Area = styled.div<IAreaProps>`
+  background-color: ${props =>
+          props.isDraggingOver
+                  ? "#dfe6e9"
+                  : props.isDraggingFromThis
+                          ? "#b2bec3"
+                          : "transparent"};
+  flex-grow: 1;
+  transition: background-color 0.5s ease-in-out;
+  padding: 20px;
+`;
+
 
 interface IBoardProps {
   toDos: string[],
@@ -23,16 +47,21 @@ const Board = ({toDos, boardId}: IBoardProps) => {
     <Wrapper>
       <Title>{boardId}</Title>
       <Droppable droppableId={boardId}>
-        {(provided, snapshot) =>
-          <div
-            ref={provided.innerRef}
-            {...provided.droppableProps}
-          >
-            {toDos.map((toDo, index) => (
-              <DraggableCard key={toDo} index={index} toDo={toDo}/>
-            ))}
-            {provided.placeholder}
-          </div>}
+        {(provided, snapshot) => {
+          return (
+            <Area
+              isDraggingOver={snapshot.isDraggingOver}
+              isDraggingFromThis={Boolean(snapshot.draggingFromThisWith)}
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+            >
+              {toDos.map((toDo, index) => (
+                <DraggableCard key={toDo} index={index} toDo={toDo}/>
+              ))}
+              {provided.placeholder}
+            </Area>
+          )
+        }}
       </Droppable>
     </Wrapper>
   )
--- src/Trello/components/DraggableCard.tsx
+++ src/Trello/components/DraggableCard.tsx
@@ -2,11 +2,17 @@ import {Draggable} from "react-beautiful-dnd";
 import React from 'react';
 import styled from "styled-components";
 
-const Card = styled.li`
-  background-color: #DADFE9;
+interface ICardProps {
+  isDragging: boolean,
+}
+
+const Card = styled.li<ICardProps>`
+  background-color: ${props => props.isDragging ? "#74b9ff" : "#DADFE9"};
+  box-shadow: ${props => props.isDragging ? "0 2px 5px rgba(0,0,0,0.5)" : "none"};
   margin-bottom: 5px;
   border-radius: 5px;
   padding: 5px 10px;
+  color: black;
 `;
 
 interface IDragabbleCardProps {
@@ -18,10 +24,12 @@ const DraggableCard = ({index, toDo}: IDragabbleCardProps) => {
 
   return (
     <Draggable draggableId={toDo} index={index}>
-      {(provided) => (
-        <Card ref={provided.innerRef}
-              {...provided.draggableProps}
-              {...provided.dragHandleProps}
+      {(provided, snapshot) => (
+        <Card
+          isDragging={snapshot.isDragging}
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
         >
           {toDo}
         </Card>
